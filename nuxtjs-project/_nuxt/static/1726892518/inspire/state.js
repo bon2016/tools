e67e760208b1efa86bb9cd832f8971eb1b335488
bon2016 <bon2016@users.noreply.github.com>
2024-09-21 04:21:59 +0000

    deploy: 89bb93faa17712da371431490006a904951de2ef

---
+++ nuxtjs-project/_nuxt/static/1726892518/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtjs-project\u002F_nuxt\u002Fstatic\u002F1726892518",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fnuxtjs-project\u002F",assetsPath:"\u002Fnuxtjs-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fc80c08c"}}}}(null));
