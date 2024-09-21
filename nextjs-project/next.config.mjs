88a0dbc5e95ea71950dc8a35263e14231f776dac
Ryotaro Yamazaki <ryotaro.merryiris@gmail.com>
2024-09-21 20:24:08 +0900

    デプロイにNuxt.jsを追加

--- nextjs-project/next.config.mjs
+++ nextjs-project/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     output: 'export',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+    publicRuntimeConfig: {
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+    },
+
 };
 
 export default nextConfig;
